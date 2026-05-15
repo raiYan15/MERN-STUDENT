d448ce78bc02ab90cc545d34aa409a107ae3f9c2
RAIYAN AZIZ <215950194+231fa04c91@users.noreply.github.com>
2026-05-16 01:42:19 +0530

    Remove all repository files

--- frontend/src/api/client.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-// Axios instance, baseURL uses Vite proxy to backend
-const client = axios.create({
-  baseURL: '/api',
-  headers: { 'Content-Type': 'application/json' },
-})
-
-export default client
